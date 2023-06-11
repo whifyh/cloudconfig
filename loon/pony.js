a2a38e715fdb030447d9d4f49e37e2ebf03c3f2e
WangChenYang <whifyh@qq.com>
2023-06-11 21:23:07 +0800

    去掉空格

--- loon/pony.js
+++ loon/pony.js
@@ -1,3 +1,3 @@
 [Script]
 enable = true;
-    http-request ^https?:\/\/(www\.)?.*myprofile.* script-path=https://raw.githubusercontent.com/whifyh/cloudconfig/main/loon/pony-response.js timeout=10 requires-body = true
+http-request ^https?:\/\/(www\.)?.*myprofile.* script-path=https://raw.githubusercontent.com/whifyh/cloudconfig/main/loon/pony-response.js timeout=10 requires-body = true
